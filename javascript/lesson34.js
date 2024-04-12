f2c2d0f678c6d8730439a0828d6dc39f73bd8b24
parampatel07 <iamparam102@gmail.com>
2024-04-12 09:02:24 +0530

    bmi

--- javascript/lesson34.js
+++ javascript/lesson34.js
@@ -1,4 +1,5 @@
 // function expression ( 2nd method )
+// footToMeter();
 
 var getInput = function () {
      var foot = parseInt(document.getElementById('foot').value);
@@ -7,13 +8,37 @@ var getInput = function () {
      var answer = [foot, inch, weight]
      console.log(` the value of foot is ${foot} inch is ${inch} and weight is ${weight} `);
      // var meter = foot / 3.281 ;
-
+     // footToMeter(foot);
+     // inchToMeter(inch);
+     // getTotalMeter(foot, inch);
+     // getBmi(weight, foot, inch);
      return answer;
 }
 
-var footToMeter = function () {
-     var answer2 = getInput();
-     console.log(answer2);
-     var meter = foot / 3.281;
+var getBmi = function () {
+     var input = getInput();
+     var bmi = input[2] / (getTotalMeter(input[0], input[1]) * getTotalMeter(input[0], input[1]));
+     console.log("the value of bmi is " + bmi);
+     return bmi;
 }
+
+var getTotalMeter = function (foot, inch) {
+     var total_meter = footToMeter(foot) + inchToMeter(inch);
+     console.log("Total meter is : " + total_meter);
+     return total_meter;
+}
+
+var footToMeter = function (foot) {
+     var foot_meter = foot / 3.281;
+     console.log("This is foot meter " + foot_meter);
+     return foot_meter;
+}
+
+
+var inchToMeter = function (inch) {
+     var inch_meter = inch / 39.37;
+     console.log("This is inch meter " + inch_meter);
+     return inch_meter;
+}
+
 // footToMeter();
